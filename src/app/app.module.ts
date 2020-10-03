1d3bf86a7c920b6f4b8a9178fc13543e376d3528
SoyamDarshan <soyam.darshan@gmail.com>
2020-10-03 21:04:03 +0530

    updated angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { TodoListModule } from "./todo-list/todo-list.module";
+import { TodoListItemComponent } from "./todo-list-item/todo-list-item.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoListItemComponent],
+  imports: [BrowserModule, AppRoutingModule, TodoListModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
